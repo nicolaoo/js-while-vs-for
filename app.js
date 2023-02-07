ea71dfedf490184ea0dfd2de60c9beafa7190fe2
nicola00 <nicolajobs00@gmail.com>
2023-02-07 15:43:22 +0100

    FEAT ciclo for

---
+++ app.js
@@ -0,0 +1,20 @@
+//Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. 
+//Se il numero è dispari, lo salviamo in una lista. 
+//Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari. 
+//Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
+//Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
+
+
+
+let numeroUser = parseInt(prompt('inserisci dei numeri'))
+
+let DispariArray = []
+
+let resto = numeroUser % 2
+
+let numDispari = (resto = 1)
+
+
+for (let i = 0; i = 10; i++){
+
+}
